1f711aa097a4d39e0d47764101c9174e71b4c451
Rafael Uchôa <rafael@overmediacast.com>
2013-05-16 19:03:33 +0200

    Hack to identify Chrome on iOS

--- lib/turbo_user_agent_parser/turbo_user_agent.rb
+++ lib/turbo_user_agent_parser/turbo_user_agent.rb
@@ -13,7 +13,14 @@ class TurboUserAgent
   end
   
   def browser
-    @user_agent.browser
+    if @user_agent.platform == 'iPhone' and @user_agent.to_s.match(/CriOS\//)
+      #'Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3'
+      # https://developers.google.com/chrome/mobile/docs/user-agent
+
+      'Chrome'
+    else
+      @user_agent.browser
+    end
   end
   
 end
--- spec/desktop_spec.rb
+++ spec/desktop_spec.rb
@@ -3,13 +3,42 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'TurboUserAgentParser to desktops user_agents strings' do
 
-  it 'should return device as Desktop, the so as Macintosh and browser as Safari fot this one' do
-    user_agent_str = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.53.11 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10'
-
-    turbo = TurboUserAgentParser.parse(user_agent_str)
-    turbo.device.should == 'Desktop'
-    turbo.os.should == 'Macintosh'
-    turbo.browser.should == 'Safari'
+  it 'should return device as Desktop and the right browser and os' do
+    
+    [
+      {
+        :ua      => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.53.11 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10',
+        :device  => 'Desktop',
+        :os      => 'Macintosh', #TODO: Macintosh é muito feio! Trocar para Mac OS
+        :browser => 'Safari',
+      },
+      {
+        :ua      => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.53.11 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10', 
+        :device  => 'Desktop',
+        :os      => 'Macintosh',
+        :browser => 'Safari'
+      },
+      {
+        :ua      => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19',
+        :device  => 'Desktop',
+        :os      => 'Macintosh',
+        :browser => 'Chrome',
+      },
+      {
+        :ua      => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19',
+        :device  => 'Desktop',
+        :os      => 'Windows',
+        :browser => 'Chrome'
+      }
+      
+    ].each do |line|
+      user_agent_str = line[:ua]
+    
+      turbo = TurboUserAgentParser.parse(user_agent_str)
+      turbo.device.should  == line[:device]
+      turbo.os.should      == line[:os]
+      turbo.browser.should == line[:browser]
+    end
   end
 
 end
