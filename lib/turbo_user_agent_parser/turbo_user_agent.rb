fa54c7b1d72f98020b658a42ba1334e482c6473c
Rafael Uchôa <rafael@overmediacast.com>
2013-05-16 21:46:26 +0200

    Identify device Mobile and os as iPhone in a new algorithm

--- lib/turbo_user_agent_parser/turbo_user_agent.rb
+++ lib/turbo_user_agent_parser/turbo_user_agent.rb
@@ -1,21 +1,35 @@
 class TurboUserAgent
-  # attr_reader :user_agent # https://github.com/josh/useragent
   attr_reader :user_agent, :device, :os, :browser
+  
+  MATCHER = %r{
+      ^([^/\s]+)        # Product
+      /?([^\s]*)        # Version
+      (\s\(([^\)]*)\))? # Comment
+    }x.freeze
 
   def initialize(user_agent)
     @user_agent = user_agent
     process!
   end
-  
+
   private
-  
+
   def process!
-    identify_os
-    # process_device!
+    @partes = @user_agent.to_s.match(MATCHER).to_a
+    
+    @os     = identify_os
+    @device = identify_device
+
     # process_browser!
   end
   
   def identify_os
-    
+    if @partes.first =~ /iPhone/
+      'iPhone'
+    end
+  end
+  
+  def identify_device
+    'Mobile' if %w{iPhone}.include? @os
   end
 end
