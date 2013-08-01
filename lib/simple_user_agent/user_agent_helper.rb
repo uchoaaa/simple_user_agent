8a527bf9afb427abbb9c29fdee3fe7feca2f37bd
Rafael Uchôa <rafael@overmediacast.com>
2013-08-01 12:40:54 -0300

    Refactor: playing around with method_mission

--- lib/simple_user_agent/user_agent_helper.rb
+++ lib/simple_user_agent/user_agent_helper.rb
@@ -1,12 +1,12 @@
 class UserAgentHelper
-  DEVICES = {
+  DEVICE = {
     :c   => 'Computer',
     :t   => 'Tablet',
     :m   => 'Mobile',
     :oth => 'Other'
   }
   
-  BROWSERS = {
+  BROWSER = {
     :sf  => 'Safari',
     :ch  => 'Chrome',
     :ff  => 'Firefox',
@@ -31,19 +31,33 @@ class UserAgentHelper
 
     :oth => 'Other'
   }
+  
+  # def self.device_name(key)
+  #   key = key || :oth
+  #   DEVICES[key.to_sym] || "Other"
+  # end
+  # 
+  # def self.browser_name(key)
+  #   key = key || :oth
+  #   BROWSERS[key.to_sym] || "Other"
+  # end
+  # 
+  # def self.os_name(key)
+  #   key = key || :oth
+  #   OS[key.to_sym] || "Other"
+  # end
 
-  def self.device_name(device_key)
-    key = device_key || :oth
-    DEVICES[key.to_sym] || "Other"
+
+  # Brincando com method_missing :D
+  def self.method_missing(method_name, *args, &block)
+    raise ArgumentError.new('list name error') if not method_name =~ /\_name$/
+    raise ArgumentError.new('wrong number of arguments (0 for 1)') if args.empty?
+
+    list_name = method_name.to_s.gsub('_name', '').upcase #device_name vira DEVICE, por exemplo
+    list = self.const_get(list_name)
+
+    key = args.first || :oth
+    list[key.to_sym] || "Other"
   end
 
-  def self.browser_name(key)
-    key = key || :oth
-    BROWSERS[key.to_sym] || "Other"
-  end
-
-  def self.os_name(key)
-    key = key || :oth
-    OS[key.to_sym] || "Other"
-  end
 end
--- spec/helpers/device_spec.rb
+++ spec/helpers/device_spec.rb
@@ -18,27 +18,27 @@ describe 'UserAgentHelper' do
       UserAgentHelper.device_name(:t).should  == 'Tablet'
       UserAgentHelper.device_name('t').should == 'Tablet'
     end
-
+    
     it 'should return "Mobile" to key :m' do
       UserAgentHelper.device_name(:m).should  == 'Mobile'
       UserAgentHelper.device_name('m').should == 'Mobile'
     end
-
+    
     it 'should return "Other" to key :oth' do
       UserAgentHelper.device_name(:oth).should  == 'Other'
       UserAgentHelper.device_name('oth').should == 'Other'
     end
-
+    
     it 'should return "Other" to any other key' do
       UserAgentHelper.device_name(:wrong).should  == 'Other'
       UserAgentHelper.device_name('other thing').should  == 'Other'
       UserAgentHelper.device_name(nil).should  == 'Other'
     end
-
+    
     it 'should raise exeception if there is no param' do
       lambda {
         UserAgentHelper.device_name
-
+    
       }.should.raise Exception
     end  
   end
