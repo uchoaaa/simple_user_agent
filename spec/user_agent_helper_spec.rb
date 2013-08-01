f212e81b3b65025f3555ef597cebacad2b9a5abe
Rafael Uchôa <rafael@overmediacast.com>
2013-08-01 11:40:43 -0300

    Helper returns the name to all devices

--- spec/user_agent_helper_spec.rb
+++ spec/user_agent_helper_spec.rb
@@ -10,12 +10,25 @@ describe 'UserAgentHelper' do
     # :oth => 'Other'
 
     it 'should return "Computer" to key :c' do
-      UserAgentHelper.device_name(:c).should == 'Computer'
+      UserAgentHelper.device_name(:c).should  == 'Computer'
       UserAgentHelper.device_name('c').should == 'Computer'
     end
 
-    # it 'should return "Tablet" to key :t'
-    # it 'should return "Mobile" to key :m'
-    # it 'should return "Other" to key :oth'
+    it 'should return "Tablet" to key :t' do
+      UserAgentHelper.device_name(:t).should  == 'Tablet'
+      UserAgentHelper.device_name('t').should == 'Tablet'
+    end
+
+    it 'should return "Mobile" to key :m' do
+      UserAgentHelper.device_name(:m).should  == 'Mobile'
+      UserAgentHelper.device_name('m').should == 'Mobile'
+    end
+
+    it 'should return "Other" to key :oth' do
+      UserAgentHelper.device_name(:oth).should  == 'Other'
+      UserAgentHelper.device_name('oth').should == 'Other'
+    end
+
+    # it 'should return "Other" to any other key'
   end
 end
