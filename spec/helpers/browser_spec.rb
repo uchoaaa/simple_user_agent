12d25aeb97c0d5ea8f9db706aca912720797c2c3
Rafael Uchôa <rafael@overmediacast.com>
2013-08-01 12:03:20 -0300

    should return "Other" to any other key

--- spec/helpers/browser_spec.rb
+++ spec/helpers/browser_spec.rb
@@ -20,28 +20,33 @@ describe 'UserAgentHelper' do
       UserAgentHelper.browser_name(:ch).should  == 'Chrome'
       UserAgentHelper.browser_name('ch').should == 'Chrome'
     end
-    
+
     it 'should return "Firefox" to key :ff' do
       UserAgentHelper.browser_name(:ff).should  == 'Firefox'
       UserAgentHelper.browser_name('ff').should == 'Firefox'
     end
-    
+
     it 'should return "Internet Explorer" to key :ie' do
       UserAgentHelper.browser_name(:ie).should  == 'Internet Explorer'
       UserAgentHelper.browser_name('ie').should == 'Internet Explorer'
     end
-    # 
-    # it 'should return "Other" to any other key' do
-    #   UserAgentHelper.browser_name(:wrong).should  == 'Other'
-    #   UserAgentHelper.browser_name('other thing').should  == 'Other'
-    #   UserAgentHelper.browser_name(nil).should  == 'Other'
-    # end
-    # 
-    # it 'should raise exeception if there is no param' do
-    #   lambda {
-    #     UserAgentHelper.browser_name
-    # 
-    #   }.should.raise Exception
-    # end  
+
+    it 'should return "Other" to key :oth' do
+      UserAgentHelper.browser_name(:oth).should  == 'Other'
+      UserAgentHelper.browser_name('oth').should == 'Other'
+    end
+    
+    it 'should return "Other" to any other key' do
+      UserAgentHelper.browser_name(:wrong).should  == 'Other'
+      UserAgentHelper.browser_name('other thing').should  == 'Other'
+      UserAgentHelper.browser_name(nil).should  == 'Other'
+    end
+    
+    it 'should raise exeception if there is no param' do
+      lambda {
+        UserAgentHelper.browser_name
+    
+      }.should.raise Exception
+    end  
   end
 end
