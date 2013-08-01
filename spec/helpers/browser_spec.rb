18321b9b9913abfe0b4b8fd161aba34d84502e76
Rafael Uchôa <rafael@overmediacast.com>
2013-08-01 12:00:29 -0300

    should return "Chrome" to key :ch

--- spec/helpers/browser_spec.rb
+++ spec/helpers/browser_spec.rb
@@ -16,30 +16,30 @@ describe 'UserAgentHelper' do
       UserAgentHelper.browser_name('sf').should == 'Safari'
     end
 
-    # it 'should return "Tablet" to key :t' do
-    #   UserAgentHelper.device_name(:t).should  == 'Tablet'
-    #   UserAgentHelper.device_name('t').should == 'Tablet'
-    # end
+    it 'should return "Chrome" to key :ch' do
+      UserAgentHelper.browser_name(:ch).should  == 'Chrome'
+      UserAgentHelper.browser_name('ch').should == 'Chrome'
+    end
     # 
     # it 'should return "Mobile" to key :m' do
-    #   UserAgentHelper.device_name(:m).should  == 'Mobile'
-    #   UserAgentHelper.device_name('m').should == 'Mobile'
+    #   UserAgentHelper.browser_name(:m).should  == 'Mobile'
+    #   UserAgentHelper.browser_name('m').should == 'Mobile'
     # end
     # 
     # it 'should return "Other" to key :oth' do
-    #   UserAgentHelper.device_name(:oth).should  == 'Other'
-    #   UserAgentHelper.device_name('oth').should == 'Other'
+    #   UserAgentHelper.browser_name(:oth).should  == 'Other'
+    #   UserAgentHelper.browser_name('oth').should == 'Other'
     # end
     # 
     # it 'should return "Other" to any other key' do
-    #   UserAgentHelper.device_name(:wrong).should  == 'Other'
-    #   UserAgentHelper.device_name('other thing').should  == 'Other'
-    #   UserAgentHelper.device_name(nil).should  == 'Other'
+    #   UserAgentHelper.browser_name(:wrong).should  == 'Other'
+    #   UserAgentHelper.browser_name('other thing').should  == 'Other'
+    #   UserAgentHelper.browser_name(nil).should  == 'Other'
     # end
     # 
     # it 'should raise exeception if there is no param' do
     #   lambda {
-    #     UserAgentHelper.device_name
+    #     UserAgentHelper.browser_name
     # 
     #   }.should.raise Exception
     # end  
